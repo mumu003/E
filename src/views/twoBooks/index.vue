5c41ee5df88c742c80c248953d8102355caca60b
ywx <youwx@xmmakeit.com>
2018-07-31 11:11:28 +0800

    工作台编辑

--- src/views/twoBooks/index.vue
+++ src/views/twoBooks/index.vue
@@ -700,9 +700,11 @@
                     this.$refs.table.init()
                   }, 2000)
                 } else {
+                  this.modal_loading = false
                   this.$Message.error(res.message)
                 }
               }, res => {
+                this.modal_loading = false
                 this.$Modal.error({title: '提示信息', content: res.message})
               })
             }else{
@@ -739,7 +741,7 @@
           this.viewData.push(res.data)
           this.viewModal = true
         },res=>{
-          this.$Message.error("获取失败")
+          this.$Message.error(res.message)
         })
         this.viewData = []
       },
